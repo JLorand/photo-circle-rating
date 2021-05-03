99fceb953a9c716fe976275bc2c2dd265b331d09
Lóránd Jakab <ljakab@graphisoft.com>
2021-05-03 22:17:12 +0200

    Image upload extra validations

--- src/components/PhotoUploadFormComponent.vue
+++ src/components/PhotoUploadFormComponent.vue
@@ -1,59 +1,76 @@
 <template>
 <div>
     <form method="POST">
-    <div class="form-group row">
-        <label for="email_address" class="col-md-4 col-form-label text-md-right">A kép címe</label>
-        <div class="col-md-6">
-            <input type="text" class="form-control" maxlength="50" v-model="title" />
-        </div>
-    </div>
 
-    <div class="form-group row">
-        <label for="email_address" class="col-md-4 col-form-label text-md-right align-middle">A kép leírása</label>
-        <div class="col-md-6">
-            <textarea class="form-control" maxlength="125" v-model="description" />
+        <div v-show="uploadSucceeded" class="alert alert-info col-md-10 offset-md-1 mb-md-5 text-center" style="font-size: 1rem;" role="alert">
+            <i class="fas fa-info-circle"></i>
+            Sikeres képfeltöltés.
         </div>
-    </div>
 
-    <div class="form-group row">
-        <label for="password" class="col-md-4 col-form-label text-md-right">Kép</label>
-        <div class="col-md-6">
-            <div class="custom-file">
-                <input type="file" class="custom-file-input" id="customFile"
-                    ref="file"
+        <div class="form-group row">
+            <label for="email_address" class="col-md-4 col-form-label text-md-right">A kép címe</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" maxlength="50" v-model="title" />
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="email_address" class="col-md-4 col-form-label text-md-right align-middle">A kép leírása</label>
+            <div class="col-md-6">
+                <textarea class="form-control" maxlength="125" v-model="description" />
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="password" class="col-md-4 col-form-label text-md-right">Kép</label>
+            <div class="col-md-6">
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="customFile"
+                        ref="file"
+                        @change="onFileChange">
+                    <label class="custom-file-label" for="customFile">{{fileInputLabel}}</label>
+                </div>
+            </div>
+            <div v-show="uploadInProgress" class="col-md-6 offset-md-4 mt-1">
+                <div class="progress">
+                    <div 
+                        :class="[progressBarColor, 'progress-bar']" 
+                        :style="{width: uploadProgress + '%'}"></div>
+                </div>
+            </div>
+        </div>
+        
+        <div v-show="showFileValidation" class="row">
+            <div class="col-12 text-center mt-4">
+                <p class="small">A kiválasztott fájl mérete nagyobb 2.5MB-nál!</p>
+            </div>
+        </div>
+
+        <div class="row" v-show="showGenericError">
+            <div class="col-12 text-center mt-4">
+                <p class="small">A feltöltés meghiúsult! Kérem, keresse fel az oldal üzemeltetőjét!</p>
+            </div>
+        </div>
+
+        <div class="row" v-show="resolutionNotAllowedError">
+            <div class="col-12 text-center mt-4">
+                <p class="small">A feltöltés meghiúsult! A kép maximális felbontása 1500x1500 pixel lehet!</p>
+            </div>
+        </div>
+        
+        <div v-show="uploadInProgress == false && uploadSucceeded == false" class="row">
+            <div class="col-12 text-center mt-4">
+                <p class="small">A feltöltés meghiúsult! Kérem, keresse fel az oldal üzemeltetőjét!</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 text-center">
+                <button type="button" class="btn-go" 
                     :disabled="uploadDisabled"
-                    @change="onFileChange">
-                <label class="custom-file-label" for="customFile">{{fileInputLabel}}</label>
+                    @click="onUpload">Feltöltés</button>
             </div>
         </div>
-        <div v-show="showFileValidation" class="col-md-6 offset-md-4 mt-1">
-            <p class="small">A kiválasztott fájl mérete nagyobb 2.5MB-nál</p>
-        </div>
-        <div v-show="uploadInProgress" class="col-md-6 offset-md-4 mt-1">
-            <div class="progress">
-                <div 
-                    :class="[progressBarColor, 'progress-bar']" 
-                    :style="{width: uploadProgress + '%'}"></div>
-            </div>
-        </div>
-        <div v-show="uploadInProgress == false && uploadSucceeded == false" class="col-md-6 offset-md-4 mt-1">
-            <p class="small">A feltöltés meghiúsult! Próbálja meg újra!</p>
-        </div>
-    </div>
-    
-    <div class="row" v-show="showGenericError">
-        <div class="col-md-10 offset-md-2 mt-4">
-            <p class="small">A feltöltés meghiúsult! Kérem, keresse fel az oldal üzemeltetőjét!</p>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12 text-center">
-            <button type="button" class="btn-go" 
-                :disabled="uploadDisabled"
-                @click="onUpload">Feltöltés</button>
-        </div>
-    </div>
     </form>
 </div>
 </template>
@@ -73,19 +90,40 @@ export default {
         const uploadInProgress = ref(false);
         const uploadProgress = ref(0);
         const uploadSucceeded = ref(null);
+        const resolutionNotAllowedError = ref(false);
         const uid = firebase.auth().currentUser.uid;
                 
         const onFileChange = (event) => {
             const uploadedFile = event?.target?.files[0];
+            showFileValidation.value = false;
+            showGenericError.value = false;
+            resolutionNotAllowedError.value = false;
+
             if(!uploadedFile){
                 selectedFile.value = null;
                 return;
             }
 
-            if(uploadedFile.size > 2500000)
+            if(uploadedFile.size > 2500000) {
                 showFileValidation.value = true;
+                selectedFile.value = null;
+                return;
+            }
 
-            showFileValidation.value = false;
+            var reader = new FileReader();
+            reader.readAsDataURL(uploadedFile);
+            reader.onload = function (e) {
+                var image = new Image();
+                image.src = e.target.result;
+                image.onload = function () {
+                    var height = this.height;
+                    var width = this.width;
+                    if (height > 1500 || width > 1500) {
+                        resolutionNotAllowedError.value = true;
+                    }
+                };
+            };
+
             selectedFile.value = uploadedFile;
         };
 
@@ -125,9 +163,7 @@ export default {
                             description: description.value,
                             circleId: usersCircleId,
                             createdDate: new Date().toISOString()
-                        }).then(() => {
-                            router.push('/home');
-                        })
+                        });
                     });
                 })
             });
@@ -148,6 +184,7 @@ export default {
             uploadSucceeded,
             showFileValidation,
             showGenericError,
+            resolutionNotAllowedError,
             onFileChange,
             onUpload
         }
@@ -155,7 +192,7 @@ export default {
     computed: {
         uploadDisabled: {
             get() {
-                return this.uploadSucceeded == true;
+                return this.uploadSucceeded == true || this.showFileValidation || this.showGenericError || this.resolutionNotAllowedError;
             }
         },
         progressBarColor: {
